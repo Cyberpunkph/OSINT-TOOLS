18456435ef2b514c96aadfa18d14f6d02ddf1f73
anonymous_PH <nollybarcela22@gmail.com>
2026-03-24 21:46:31 +0800

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,29 +1,66 @@
 #!/usr/bin/env ruby
+
 require 'optparse'
 require 'colorize'
+
 require_relative 'modules/username'
+require_relative 'modules/dns'
+require_relative 'modules/ip'
+require_relative 'modules/email'
+require_relative 'modules/subdomain'
 require_relative 'utils/report'
 
 options = {}
 
 OptionParser.new do |opts|
   opts.banner = "Usage: ruby main.rb [options]"
+
   opts.on("-n USERNAME", "Username OSINT") { |v| options[:username] = v }
+  opts.on("-d DOMAIN", "Domain OSINT") { |v| options[:domain] = v }
+  opts.on("-i IP", "IP Lookup") { |v| options[:ip] = v }
+  opts.on("-u URL", "Email Scraper") { |v| options[:url] = v }
   opts.on("--tor", "Use TOR") { options[:tor] = true }
 end.parse!
 
-puts "RUBY OSINT TOOLKIT v4.0".blue
+puts "RUBY OSINT TOOLKIT v4.1".blue
+
+results = {}
+
+# =========================
+# EXECUTION
+# =========================
 
 if options[:username]
-  results = username_check(options[:username], use_tor: options[:tor])
-  Dir.mkdir("output") unless Dir.exist?("output")
-
-  File.write("output/report.json", JSON.pretty_generate(results))
-  File.write("output/report.txt", results.map { |r| r[:url] }.join("\n"))
-
-  generate_html(results)
-
-  puts "\nReports saved in /output".yellow
-else
-  puts "Use -h for help".red
+  results[:username] = username_check(options[:username], use_tor: options[:tor])
 end
+
+if options[:domain]
+  results[:dns] = dns_lookup(options[:domain])
+  results[:subdomains] = subdomain_enum(options[:domain])
+end
+
+if options[:ip]
+  results[:ip] = ip_lookup(options[:ip])
+end
+
+if options[:url]
+  results[:emails] = email_scraper(options[:url])
+end
+
+# =========================
+# OUTPUT
+# =========================
+
+if results.empty?
+  puts "Use -h for help".red
+  exit
+end
+
+Dir.mkdir("output") unless Dir.exist?("output")
+
+File.write("output/report.json", JSON.pretty_generate(results))
+File.write("output/report.txt", results.to_s)
+
+generate_html(results)
+
+puts "\nReports saved in /output".yellow
