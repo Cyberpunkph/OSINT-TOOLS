d544baed15fa374eacdc44ab0eb67d4aaad7487e
anonymous_PH <nollybarcela22@gmail.com>
2026-03-24 22:11:51 +0800

    Update detector.rb

--- utils/detector.rb
+++ utils/detector.rb
@@ -1,8 +1,20 @@
 def detect_account(response, site)
   code = response.code.to_i
+  body = response.body.to_s
+
   case site["errorType"]
   when "status_code"
     code == 200
+
+  when "message"
+    !body.include?(site["errorMsg"])
+
+  when "redirect"
+    [200, 301, 302].include?(code)
+
+  when "regex"
+    !(body =~ Regexp.new(site["errorMsg"]))
+
   else
     false
   end
