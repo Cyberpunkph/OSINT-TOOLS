ffd8636f84456224e1c58a1567cda8d69640cf51
anonymous_PH <nollybarcela22@gmail.com>
2026-03-24 22:02:33 +0800

    Update username.rb

--- modules/username.rb
+++ modules/username.rb
@@ -1,34 +1,45 @@
 require 'json'
 require 'colorize'
 require 'concurrent'
+
 require_relative '../utils/http'
 require_relative '../utils/detector'
 
 def username_check(username, use_tor: false)
-  puts "\n[USERNAME OSINT v4]".green
+  puts "\n[USERNAME OSINT v5 - HIGH SPEED]".green
+
   sites = JSON.parse(File.read("config/sites.json"))
+
   found = []
   mutex = Mutex.new
 
-  futures = sites.map do |site|
-    Concurrent::Future.execute do
+  # 🔥 HIGH PERFORMANCE THREAD POOL
+  pool = Concurrent::FixedThreadPool.new(20)
+
+  sites.each do |site|
+    pool.post do
       url = site["url"].gsub("{username}", username)
+
       begin
         res = http_request(url, use_tor: use_tor)
+
         if detect_account(res, site)
           mutex.synchronize do
-            puts "[FOUND] #{site['name']} -> #{url}".green
+            puts "[FOUND] #{site['name']}".green
             found << { site: site["name"], url: url }
           end
-        else
-          puts "[NOT FOUND] #{site['name']}".red
         end
+
       rescue
-        puts "[ERROR] #{site['name']}".yellow
+        # silent fail for speed
       end
     end
   end
 
-  futures.each(&:wait)
+  pool.shutdown
+  pool.wait_for_termination
+
+  puts "\nScan complete: #{found.size} hits".cyan
+
   found
 end
